7f6492a4e233958ea61125a5d4a34e16df667d33
wyctorfogos <wyctor.fogos@gmail.com>
2024-11-16 10:02:21 -0300

    perf: Add app to put the website on live mode

--- server/server.js
+++ server/server.js
@@ -1,5 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
 
-app.listen(port, () => console.log(`Server is running and listening on port http://0.0.0.0:${port}!`))
+const app = express();
+const PORT = 3002;
+
+// Define a pasta 'public' como raiz para os arquivos estáticos
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Rota padrão para a página inicial
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/src/views/index.html"));
+});
+
+// Rota para a página 'contact'
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/src/views/contact.html"));
+  });
+
+// Rota para a página 'about'
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/src/views/about.html"));
+  });
+
+// Rota para a página 'professional_expirience'
+app.get("/professional_expirience", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/src/views/professional_expirience.html"));
+  });
+
+// Rota para a página 'about'
+app.get("/projects", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/src/views/projects.html"));
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
